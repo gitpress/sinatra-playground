fc5feb004ab610f879e0b241ad7b2b1c574564da
GrilloPress <grillopress@gmail.com>
2016-02-10 23:24:25 +0000

    add simple csv and csv reader route

--- csv_me/server.rb
+++ csv_me/server.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+# require "sinatra/reloader"
+require 'csv'
 # set bind a codio addition to force port to be public
 set :bind, '0.0.0.0'
 
+
 get '/' do
-  erb :index
+  contents = CSV.open "test.csv", headers: true, header_converters: :symbol
+  erb :index, :locals => { :contents => contents }
 end

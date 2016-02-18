b8e3c715e4ab65b6334c8b51c38f279324698783
GrilloPress <grillopress@gmail.com>
2016-02-18 10:22:49 +0000

    add mini exmaples to how do apps

--- howdo/server.rb
+++ howdo/server.rb
@@ -1,12 +1,33 @@
 require 'sinatra'
 # set bind a codio addition to force port to be public
 set :bind, '0.0.0.0'
+set :port, 3000
 
 get '/' do
   'How do, Sinatra!'
+end
+
+get '/my_name_is/:name' do
+  
+  name = params[:name]
+  if (name.downcase.include?("andrew"))
+    "YES"
+  else
+    "How the hell did you find this?"
+  end
+  
 end
 
 get '/:name/:message' do
 
   "How do #{params[:name]}, #{params[:message]} to you too!"
 end
+
+get '/thingy/:test/:another' do 
+  
+  test = params[:test]
+  another = params[:another]
+  erb :index, :locals => { :test => test, :another => another }
+  
+end
+
--- howdo/times.rb
+++ howdo/times.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 # set bind a codio addition to force port to be public
 set :bind, '0.0.0.0'
+set :port, 3000
 
 get '/:first/:second' do
   @first = params[:first].to_i
